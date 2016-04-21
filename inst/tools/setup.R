3cd58e5240f531d5a6b531cc4c5f1bec9024f6c2
John Chambers <jmc@stat.stanford.edu>
2016-04-20 17:12:57 -0700

    example tools/setup.R

--- inst/tools/setup.R
+++ inst/tools/setup.R
@@ -2,3 +2,7 @@ roxygen2::roxygenize()
 ## assumes XR, XRPython parallel directories, should check
 ndoc <- readLines("../XR/man/noscalar.Rd")
 writeLines(ndoc, "./man/noscalar.Rd")
+proxyFile <- file("/tmp/proxyClasses.R", "w")
+setPythonClass("list", save = proxyFile)
+setPythonClass("dict", save = proxyFile)
+close(proxyFile)
