a48f13a1c8cd5e0a857b3e1db02fcedf8da6b046
John Chambers <jmc@stat.stanford.edu>
2016-04-21 10:25:26 -0700

    minor

--- inst/tools/setup.R
+++ inst/tools/setup.R
@@ -2,6 +2,8 @@ roxygen2::roxygenize()
 ## assumes XR, XRPython parallel directories, should check
 ndoc <- readLines("../XR/man/noscalar.Rd")
 writeLines(ndoc, "./man/noscalar.Rd")
+## this writes a version in /tmp that has to be hand-edited to add
+## roxygen-style documentation.
 proxyFile <- file("/tmp/proxyClasses.R", "w")
 setPythonClass("list", save = proxyFile)
 setPythonClass("dict", save = proxyFile)
