dec0b3d5f98e4216026cd8ecec4a2bb133221879
John Chambers <jmc@stat.stanford.edu>
2016-04-29 12:38:26 -0700

    package setup example

---
+++ inst/tools/listMethods.R
@@ -0,0 +1,11 @@
+### additional methods for the list_Python proxy class
+
+list_Python$methods(
+    el = function(i) {
+        'Extract an element from the list (zero based indexing).
+The index will be coerced to integer (unless a proxy).'
+        if(is.numeric(i))
+            i <- as.integer(i)
+        Eval("%s[%s]", .self, i)
+    }
+)
--- inst/tools/setup.R
+++ inst/tools/setup.R
@@ -1,9 +1,18 @@
-## write a version in /tmp  for testing
-proxyFile <- file("/tmp/proxyClasses.R", "w")
+## See ?setupStep in the XRPython documentation for a discussion of this script.
+## This script should only be run through XR::packageSetup()
+target <- "R/pythonProxyClasses.R"
+if(file.exists(target) && any(grepl("list_Python", readLines(target)))) {
+    ## second stage:  create the documentation from roxygen
+
+    roxygen2::roxygenize()
+    ## assumes XR, XRPython parallel directories, should check
+    ndoc <- readLines("../XR/man/noscalar.Rd")
+    writeLines(ndoc, "./man/noscalar.Rd")
+}
+proxyFile <- file("R/pythonProxyClasses.R", "w")
 setPythonClass("list", save = proxyFile, docText = readLines("inst/tools/listDoc.txt"))
+writeLines(readLines("inst/tools/listMethods.R"), proxyFile)
 setPythonClass("dict", save = proxyFile, docText = readLines("inst/tools/dictDoc.txt"))
 close(proxyFile)
-roxygen2::roxygenize()
-## assumes XR, XRPython parallel directories, should check
-ndoc <- readLines("../XR/man/noscalar.Rd")
-writeLines(ndoc, "./man/noscalar.Rd")
+
+## first time, list_Python will not be defined & so could not be exported
