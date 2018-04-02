9a6d20a4dc5a4c7f67e3b112b2b3798b42aa1b2f
John Chambers <jmc@stat.stanford.edu>
2018-04-02 15:38:42 -0700

    introduce forR() version of serverEval

--- inst/python/RPython.py
+++ inst/python/RPython.py
@@ -214,7 +214,7 @@ def unpickle_for_R(obj):
 
 def value_for_R(expr, key, send):
     code = None
-    if key == "":
+    if key == "" or key == "forR":
         what = "command"
         comp = 'exec'
     else:
@@ -226,10 +226,13 @@ def value_for_R(expr, key, send):
         return error_for_R("Compile error in {0} \"{1}\": {2}".format(what, expr, sys.exc_value))
     obj = None
     try:
-        obj = eval(code, globals(), _for_R)
+        if key == "forR" :
+            eval(code, _for_R)
+        else:
+            obj = eval(code, globals(), _for_R)
     except:
         return error_for_R("Evaluation error in {0} \"{1}\": {2}".format(what, expr, sys.exc_value))
-    if key == "":
+    if key == "" or key == "forR":
         return 'null'
     return proxy_or_object(obj, send, key)
 
